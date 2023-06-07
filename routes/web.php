01ca65043462a24628dc2af3aa1aaf8ec674eb65
Farrel Adel Mohammad <farrel.adel@gmail.com>
2023-06-07 14:50:38 +0700

    commit pertama (fitur read database)

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StudentController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/student', [StudentController::class, 'index']);
+Route::get('/student/create', [StudentController::class, 'create']);
+Route::post('/student/create', [StudentController::class, 'store'])->name('studentStore');
