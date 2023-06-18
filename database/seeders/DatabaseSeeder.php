1bba9eb0a23ebecb79a22ba28e4fffced5384ee5
Farrel Adel Mohammad <farrel.adel@gmail.com>
2023-06-18 19:37:13 +0700

    update student.index, seed student

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        Student::factory(10)->create();
+        $this->call([StudentSeeder::class]);
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
--- resources/views/student/index.blade.php
+++ resources/views/student/index.blade.php
@@ -1,20 +1,26 @@
 @extends('template.template')
 @section('title', 'Data Mahasiswa')
 @section('body')
+<h2 class="text-center my-5">Data Mahasiswa</h2>
 <div class="container my-5 border rounded p-2 px-5">
     <a class="btn btn-primary my-4" href="/student/create">Tambah data</a>
 
     <table class="table ">
         <thead>
             <tr>
+                <th>#</th>
                 <th>NPM</th>
                 <th>Nama</th>
                 <th>Action</th>
             </tr>
         </thead>
         <tbody>
+            @php
+                $row_num = 0;
+            @endphp
             @foreach ($students as $student)
             <tr>
+                <th>{{++$row_num}}</th>
                 <th>{{ $student['npm'] }}</th>
                 <td>{{ $student['nama'] }}</td>
                 <td>
